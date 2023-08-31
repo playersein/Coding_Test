19c92343d45d55e67203df6904a137e4dfcefd72
playersein <125570793+playersein@users.noreply.github.com>
2023-08-31 11:07:33 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
@@ -0,0 +1,12 @@
+def solution(s):
+    result = []
+    
+    if len(s) == 4 or len(s) == 6:
+        for a in s:
+            if a in [str(i) for i in range(0,10)]:
+                result.append(a)
+        if len(result) == len(s):
+            return True
+        else:
+            return False
+    return False
